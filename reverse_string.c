33340fbf18198629565af13c5434509e69e7c7c5
WuPenglin <WuPenglin@users.noreply.github.com>
2016-06-16 11:03:20 +0800

    reverse_string.c
    [LeetCode] 344. Reverse String

---
+++ reverse_string.c
@@ -0,0 +1,16 @@
+char* reverseString(char* s) {
+    char *h = s;
+    char *t = s;
+    char ch;
+    
+    while(*t++) {}
+    *t--;
+    *t--;
+    
+    while(h < t) {
+        ch = *h;
+        *h++ = *t;
+        *t-- = ch;
+    }
+    return s;
+}
